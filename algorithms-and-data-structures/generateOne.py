e6fb1718773d66e6dca387841d499d7fd6a2febe
Boya Chiou <chinawhitewhy@gmail.com>
2016-01-18 23:33:30 +0900

    Update generateOne.py

--- algorithms-and-data-structures/generateOne.py
+++ algorithms-and-data-structures/generateOne.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import random
 
 def generateOne(strlen):
@@ -10,5 +11,28 @@ def generateOne(strlen):
 
 #print generateOne()
 
-def score(goal, teststring):
+def score(goal,teststring):
+	numSame = 0
+	for i in range(len(goal)):
+		if goal[i] == teststring[i]:
+			numSame = numSame + 1
+	return numSame / len(goal) 
+
+#print score('methinks it is like a result', generateOne(28))
+def main():
+	#goalstring = 'methinks it is like a weasel'
+	goalstring = 'alexis is pig'
+	newstring = generateOne(13)
+	newscore = score(goalstring,newstring)
+	best = 0
+	count = 0
 	
+	while newscore <= 1:
+		if newscore > best:
+			print count, best, newscore, newstring
+			count = count + 1
+			best = newscore
+		newstring = generateOne(13)
+		newscore = score(goalstring,newstring)
+		
+main()
