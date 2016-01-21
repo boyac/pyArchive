d50dda9122d2fa4ab183675f023cda5eb4931a80
Boya Chiou <chinawhitewhy@gmail.com>
2016-01-21 18:54:31 +0900

    Create anagram_checkoff.py
    simple condition of checking off list, both s1, s2 will be nested lists

---
+++ anagram_checkoff.py
@@ -0,0 +1,24 @@
+combo = ['Amy', 'Bob']
+re = ['Bob', 'Amy', 'Dating']
+
+def connect(s1,s2):
+    pos1 = 0
+    new = []
+    while pos1< len(s1):
+        pos2 = 0
+        while pos2 < len(s2):
+            if s1[pos1] == s2[pos2]:
+                new.append(s1[pos1])
+                break
+            else:
+                pos2 = pos2 + 1
+        pos1 = pos1 + 1
+    
+    if len(new) == 2:
+        new.append(re[2])
+    return new 
+
+print(connect(combo, re))
+
+
+
