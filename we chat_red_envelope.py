c47656aae971e9d45e7478dc1ce01a03ce93e51c
Boya Chiou <chinawhitewhy@gmail.com>
2016-02-12 15:40:17 +0900

    Update we chat_red_envelope.py

--- we chat_red_envelope.py
+++ we chat_red_envelope.py
@@ -1,3 +1,9 @@
+＃Red envelope lucky draw
+"""
+The user assigns a lump sum to a group red envelope, 
+and the number of small red envelopes within it. 
+After posting to a group chat, WeChat will randomly assign the amount in each envelope to each recipient
+"""
 import numpy as np, numpy.random
 def money(amount, n):
     return np.random.dirichlet(np.ones(n),size=1) * amount # data clean 就不處理了
