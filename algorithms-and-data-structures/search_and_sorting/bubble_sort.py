23c9e3f3ed4d0ecbee0224224525d31a5c782b56
Boya Chiou <chinawhitewhy@gmail.com>
2016-04-08 23:13:41 +0900

    Create bubble_sort.py

---
+++ algorithms-and-data-structures/search_and_sorting/bubble_sort.py
@@ -0,0 +1,12 @@
+#ref: https://www.youtube.com/watch?v=P00xJgWzz2c
+
+def bubble(lst):
+    for j in range(len(lst)-1,0,-1): # range(start, end, step)
+        for i in range(0,j):
+            if lst[i] > lst[i+1]:
+            	lst[i],lst[i+1] = lst[i+1],lst[i] #if List[i] > List[i+1], swaps position
+    return lst
+
+
+lst = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
+print bubble(lst)
