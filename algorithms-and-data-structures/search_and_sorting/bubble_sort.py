d675e2a9a587bcaf5c6be7ca7aefa15d760f77c6
Boya Chiou <chinawhitewhy@gmail.com>
2016-04-08 23:26:07 +0900

    Update bubble_sort.py

--- algorithms-and-data-structures/search_and_sorting/bubble_sort.py
+++ algorithms-and-data-structures/search_and_sorting/bubble_sort.py
@@ -1,6 +1,6 @@
 #ref: https://www.youtube.com/watch?v=P00xJgWzz2c
 
-def bubble(lst):
+def bubble_sort(lst):
     for j in range(len(lst)-1,0,-1): # range(start, end, step)
         for i in range(0,j):
             if lst[i] > lst[i+1]:
@@ -9,4 +9,4 @@ def bubble(lst):
 
 
 lst = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
-print bubble(lst)
+print bubble_sort(lst)
