9abc5bbe4797ed7ebe4395b0b310f8d11d8ec231
Boya Chiou <chinawhitewhy@gmail.com>
2016-04-08 23:50:56 +0900

    Update selection_sort.py

--- algorithms-and-data-structures/search_and_sorting/selection_sort.py
+++ algorithms-and-data-structures/search_and_sorting/selection_sort.py
@@ -1,12 +1,19 @@
 # ref: https://www.youtube.com/watch?v=6nDMgr0-Yyo
+# search for the smallest element and swap it into the first position
+# and search rest of the array and again finding the smallest element and swap into the 2nd postion
 
 def selection_sort(lst):
-	index = len(lst) - 1
+	index = len(lst) - 1 # index of remaining unsorted list
 	while index:
-		max_index = index
+		min_index = index
 		for i in range(index):
-			if lst[i] > lst[max_index]:
-				max_index = i
-		if lst[max_index] > lst[index]:
-			lst[index], lst[max_index] = lst[max_index], lst[index]
+			if lst[i] > lst[min_index]:
+				min_index = i
+		if lst[min_index] > lst[index]:
+			lst[index], lst[min_index] = lst[min_index], lst[index]
 		index -= 1
+	return lst
+
+
+lst = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
+print selection_sort(lst)
