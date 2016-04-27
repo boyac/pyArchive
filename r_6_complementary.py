ce3ffaa68edac288494888f338f00b874f52392e
Boya Chiou <chinawhitewhy@gmail.com>
2016-04-27 22:03:29 +0900

    Update r_6_complementary.py

--- r_6_complementary.py
+++ r_6_complementary.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: boyac
+# @Date:   2016-04-27 20:40:29
+# @Last Modified by:   boyac
+# @Last Modified time: 2016-04-27 21:52:54
+
 A = [(0, 8),(1,6),(4,8),(5,5),(6,1),(8,0),(8,4)]
 A_value = [1, 8, -3, 0, 1, 3, -2, 4, 5]
 
