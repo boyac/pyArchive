f4de0c57cdcfd88cdb91f1218b05c59c6cdd9910
Boya Chiou <chinawhitewhy@gmail.com>
2016-04-27 22:28:34 +0900

    Update r_6_complementary.py

--- r_6_complementary.py
+++ r_6_complementary.py
@@ -4,8 +4,8 @@
 # @Last Modified by:   boyac
 # @Last Modified time: 2016-04-27 21:52:54
 
-A = [(0, 8),(1,6),(4,8),(5,5),(6,1),(8,0),(8,4)]
-A_value = [1, 8, -3, 0, 1, 3, -2, 4, 5]
+A = [(0, 8),(1,6),(4,8),(5,5),(6,1),(8,0),(8,4)] # index number pairing
+A_value = [1, 8, -3, 0, 1, 3, -2, 4, 5] # corresponding values
 
 Klist = []
 for i, j in A:
