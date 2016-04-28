ba64581c6e06166f910193ed786e3a5a74fef457
Boya Chiou <chinawhitewhy@gmail.com>
2016-04-28 12:19:17 +0900

    Create r_rectilinear_2.py

---
+++ r_rectilinear_2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author: boyac
+# @Date:   2016-04-27 20:40:29
+# @Last Modified by:   boyac
+# @Last Modified time: 2016-04-27 21:52:54
+
+def area(K, L, M, N, P, Q, R, S):
+	area = (M-K)*(N-L)+(R-P)*(S-Q)
+	overlap = (M-P)*(S-L)
+	if area <= 2147483647:
+		return [area if (P>M or S<L) else (area-overlap)]
+	else:
+		return -1
+		
+print area(-4, 1, 2, 6, 0, -1, 4, 3)
+
+
