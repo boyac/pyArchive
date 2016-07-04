e4108fbb33e21546c745de0ca22bfd2886ce33e3
Boya Chiou <chinawhitewhy@gmail.com>
2016-07-04 20:13:22 +0900

    Update viz.py

--- machine_learning/viz.py
+++ machine_learning/viz.py
@@ -49,11 +49,11 @@ from sklearn.externals.six import StringIO
 import pydot
 dot_data = StringIO()
 tree.export_graphviz(clf, 
-						out_file=dot_data,
-						feature_names=iris.feature_names,
-						class_names=iris.target_names,
-						filled=True, rounded=True,
-						impurity=False)
+			out_file=dot_data,
+			feature_names=iris.feature_names,
+			class_names=iris.target_names,
+			filled=True, rounded=True,
+			impurity=False)
 
 graph = pydot.graph_from_dot_data(dot_data.getvalue())
 graph.write_pdf('iris.pdf')
