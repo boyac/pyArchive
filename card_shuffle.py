ed1dcfcd18cd2e0c4c1f6e8a4fd707c425f05153
Boya Chiou <chinawhitewhy@gmail.com>
2016-07-06 17:50:11 +0900

    Delete card_shuffle.py

--- card_shuffle.py
+++
@@ -1,8 +0,0 @@
-# Mission: Program a computer to shuffle a deck of cards
-
-import itertools
-import random
-
-deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
-random.shuffle(deck)
-print ('You got: {}'.format(deck[0:5])) # shuffle a deck of card randomly and deal 5 cards each time
