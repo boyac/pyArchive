02c3958b8ca2639b7e7256ae45edc6e020994f62
Boya Chiou <chinawhitewhy@gmail.com>
2016-07-11 09:19:57 +0900

    Update oop_03.py

--- OOP/oop_03.py
+++ OOP/oop_03.py
@@ -1,5 +1,5 @@
 class JustCounter:
-   __secretCount = 0
+   __secretCount = 0 #__ two underscore mean private attribute and can't be accessed from outside
   
    def count(self):
       self.__secretCount += 1
